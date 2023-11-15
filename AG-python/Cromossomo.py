d767efbeaf50d00ef8e175351778141a9859b58d
Bernardo <bernardodesouviero@gmail.com>
2023-11-15 17:31:41 -0300

    tarefa ok

--- AG-python/Cromossomo.py
+++ AG-python/Cromossomo.py
@@ -1,24 +1,22 @@
 class Cromossomo:
     def __init__(self, valor, estado_final):
         self.valor = valor
-        self.aptidao = calcular_aptidao(estado_final)
+        self.aptidao = self.calcular_aptidao(estado_final)
+        self.porcentagem_aptidao = 0
 
     def calcular_aptidao(self, estado_final):
+        nota = 0
         for i in range(len(estado_final)):
-            nota = 0
-            if(estado_final[i] in self.valor):
+            if self.valor.__contains__(estado_final[i]):
                 nota += 5
-            if(self.valor[i] == estado_final[i]):
+            if self.valor[i] == estado_final[i]:
                 nota += 50
         return nota
-    
+
+    def __lt__(self, other):
+        return self.aptidao > other.aptidao
+
     def __eq__(self, other):
         if isinstance(other, Cromossomo):
             return self.valor == other.valor
         return False
-
-    def __gt__(self,other):
-        return self.aptidao <= other.aptidao
-
-    def __str__(self):
-        return "valor= " + str(self.valor) + ", aptidao = " + str(self.aptidao)
